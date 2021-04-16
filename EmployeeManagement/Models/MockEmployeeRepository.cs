9f17d3f82a08f49184fb927cac284a09dd3b2168
nicole929chan <nicole929chan@gmail.com>
2021-04-16 14:02:00 +0800

    加入專案檔案。

---
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{    
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "Nick", Department = "HR", Email = "nick@example.com"},
+                new Employee() { Id = 2, Name = "Simon", Department = "HR", Email = "simon@example.com"}
+            };
+        }
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
