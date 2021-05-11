fc945cef75db0f26e0cb15fe3beee345d460385b
nicole929chan <nicole929chan@gmail.com>
2021-05-11 15:39:07 +0800

    edit form

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -46,12 +46,80 @@ namespace EmployeeManagement.Controllers
             return View();
         }
 
+        [HttpGet]
+        public ViewResult Edit(int id)
+        {
+            Employee employee = _employeeRepository.GetEmployee(id);
+            EmployeeEditViewModel employeeEditViewModel = new EmployeeEditViewModel
+            {
+                Id = employee.Id,
+                Name = employee.Name,
+                Email = employee.Email,
+                Department = employee.Department,
+                ExistingPhotoPath = employee.PhotoPath
+            };
+
+            return View(employeeEditViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EmployeeEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Employee employee = _employeeRepository.GetEmployee(model.Id);
+                employee.Name = model.Name;
+                employee.Email = model.Email;
+                employee.Department = model.Department;
+                if (model.Photo != null)
+                {
+                    // 刪除舊圖檔
+                    if (model.ExistingPhotoPath != null)
+                    {
+                        string filePath = Path.Combine(webHostEnvironment.WebRootPath, 
+                            "images", model.ExistingPhotoPath);
+                        System.IO.File.Delete(filePath);
+                    }
+
+                    // 重新上傳圖檔
+                    employee.PhotoPath = ProcessUploadedFile(model);
+                }               
+
+                // 員工資料真正更新到資料庫
+                _employeeRepository.Update(employee);
+                
+                return RedirectToAction("index");
+            }
+
+            return View();
+        }
+
+        private string ProcessUploadedFile(EmployeeCreateViewModel model)
+        {
+            string uniqueFileName = null;
+            if (model.Photo != null)
+            {
+                // 資料夾路徑
+                string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+
+                // 檔名
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
+
+                // 完整路徑
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                // 上傳檔案
+                model.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
+            }
+            return uniqueFileName;
+        }
+
         [HttpPost]
         public IActionResult Create(EmployeeCreateViewModel model)
         {
             if (ModelState.IsValid)
             {
-                string uniqueFileName = null;
+                string uniqueFileName = ProcessUploadedFile(model);
                 if (model.Photo != null)
                 {
                     // 資料夾路徑
